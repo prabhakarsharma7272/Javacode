e928c17c701bb7d5107413941cae5d689a31e049
prabhakarsharma7272 <prabhakarsharma5723@gmail.com>
2024-02-25 15:36:59 +0530

    count

--- DSA/Maths/count.java
+++ DSA/Maths/count.java
@@ -1,8 +1,28 @@
 package DSA.Maths;
 
+import java.util.Scanner;
+
 public class count {
     public static void first(int n){
+        int x = n; 
+        int count =0;
+        while (x>0) {
+           int y = x%10;
+           if ( y !=0 && n%y==0) {
+            count++;
+           }
+           x=x/10;
+            
+        }
+        System.out.println(count);
         
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        first(n);
+        sc.close();
     }
     
 }
